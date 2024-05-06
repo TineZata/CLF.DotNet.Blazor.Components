52ce120e0f9ffde3f85b584e932a0663d550096d
Tine Zata <60703214+TineZata@users.noreply.github.com>
2024-05-06 22:17:26 +0100

    added PVWidgetBase

--- Clf.Blazor.Basic.Components/Controls/WidgetBase.cs
+++ Clf.Blazor.Basic.Components/Controls/WidgetBase.cs
@@ -10,41 +10,44 @@ using System.Threading.Tasks;
 
 namespace Clf.Blazor.Basic.Components.Controls
 {
-  public class WidgetBase : ComponentBase
-  {
-    [Parameter]
-    public string Class { get; set; } = "";
-
-    [Obsolete]
-    [Parameter]
-    public bool IsPositionRelative { get; set; } = true;
-
-    [Obsolete]
-    [Parameter]
-    public string Name { get; set; } = string.Empty;
-
-    [Parameter]
-    public bool IsVisible { get; set; } = true;
-
-    [Obsolete]
-    [Parameter]
-    public int Width { get; set; }
-
-    [Obsolete]
-    [Parameter]
-    public int Height { get; set; }
-
-    [Obsolete]
-    [Parameter]
-    public string FontStyle { get; set; } = Models.FontStyle.DEFAULT_FONT_STYLE;
-
-    public void OnPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    public class WidgetBase : ComponentBase
     {
-      InvokeAsync(StateHasChanged);
+        [Parameter]
+        public string Class { get; set; } = "";
+
+        [Obsolete]
+        [Parameter]
+        public bool IsPositionRelative { get; set; } = true;
+
+        [Obsolete]
+        [Parameter]
+        public string Name { get; set; } = string.Empty;
+
+        [Parameter]
+        public bool IsVisible { get; set; } = true;
+
+        [Parameter]
+        public int Width { get; set; }
+
+        [Parameter]
+        public int Height { get; set; }
+
+        [Obsolete]
+        [Parameter]
+        public string FontStyle { get; set; } = Models.FontStyle.DEFAULT_FONT_STYLE;
+
+        [Parameter]
+        public string TooltipText { get; set; } = string.Empty;
+        [Parameter]
+        public bool ShowTooltip { get; set; } = false;
+
+        public void OnPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            InvokeAsync(StateHasChanged);
+        }
+        public void OnItemsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+        {
+            InvokeAsync(StateHasChanged);
+        }
     }
-    public void OnItemsCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-    {
-      InvokeAsync(StateHasChanged);
-    }
-  }
 }
